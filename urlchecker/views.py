bdee380884869fdd67d4d21c35961ae09fd14cd3
AbouOpenSource <aboudra1996@gmail.com>
2020-12-02 00:57:27 +0100

    adding gnucorn

--- urlchecker/views.py
+++ urlchecker/views.py
@@ -12,6 +12,5 @@ def resutl(request):
     model = joblib.load("finalized_model.sav")
     data = utils.pre_processing_of_query(request.GET['query'])
     answer = model.predict([data])
- 
     return render(request, "urlchecker/result.html",{"answer": answer,
                                                      "query" : request.GET['query'] })
